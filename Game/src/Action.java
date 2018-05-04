e5d06c9e6ef8e2be9ce5c97b9b3d6b0f90b6e708
meesf <m.froberg@students.uu.nl>
2018-05-04 15:39:56 +0200

    initial commit

---
+++ Game/src/Action.java
@@ -0,0 +1,12 @@
+/**
+ * This class represents a territory in the game.
+ * @author Games&AgentsGroup8
+ * @version FirstPrototype
+ * @date 4/5/2018
+ */
+public class Action {
+
+	public Action() {
+		
+	}
+}
---
+++ Game/src/RiskPhysics.java
@@ -0,0 +1,12 @@
+/**
+ * This class contains the data of the game and methods that manipulate this data.
+ * @author Games&AgentsGroup8
+ * @version FirstPrototype
+ * @date 4/5/2018
+ */
+public class RiskPhysics {
+
+	public RiskPhysics() {
+		
+	}
+}
