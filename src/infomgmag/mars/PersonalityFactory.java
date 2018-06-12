93199d6c31f2a157251a73c98977a20b6c670086
Uwila <Uwila@users.noreply.github.com>
2018-06-12 15:23:22 +0200

    Add continentPersonality

--- src/infomgmag/mars/PersonalityFactory.java
+++ src/infomgmag/mars/PersonalityFactory.java
@@ -49,10 +49,27 @@ public class PersonalityFactory {
                 0.5,
                 20.0,
                 4.0,
-                50.0,
+                40.0,
                 20.0,
                 0.05,
                 4);
     }
-    
+
+    public static Personality continentPersonality() {
+        return new Personality(
+                "Continent",
+                120.0,
+                170.0,
+                1.2,
+                -0.3,
+                0.05,
+                -0.03,
+                0.5,
+                20.0,
+                4.0,
+                80.0,
+                40.0,
+                0.05,
+                4);
+    }
 }
